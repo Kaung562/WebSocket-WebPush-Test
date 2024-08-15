f6258c72631dbc5492acf1b7c4ee88d9891d1869
Kaung562 <120101980+Kaung562@users.noreply.github.com>
2024-08-15 22:27:09 +0630

    first commit

---
+++ src/main/java/com/test/webnoti/WebnotiApplication.java
@@ -0,0 +1,13 @@
+package com.test.webnoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebnotiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebnotiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/webnoti/WebnotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.webnoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
